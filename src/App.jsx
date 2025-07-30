a1bb84afd04fa1ed7103434c9dabf8352283c256
impossible <nsrfars8@gmail.com>
2025-07-30 17:11:32 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setPosts(res.data));
+  }, []);
+  return (
+    <>
+      <div style={{ width:"700px" ,marginLeft:"300px"}}>
+        {posts.slice(0, 10).map((post) => {
+          return (
+            <div
+              key={post.id}
+              style={{
+                color: "black",
+                border: "2px solid blue",
+                margin: "10px",
+                padding: "10px",
+                backgroundColor: "white",
+              }}
+            >
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
